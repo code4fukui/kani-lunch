ff7f47c6d321bd71d9f6fb0ce98419b87674a052
Taisuke Fukuno <fukuno@jig.jp>
2022-10-07 06:55:03 +0900

    delete unused file

--- chg.js
+++
@@ -1,7 +0,0 @@
-import { CSV } from "https://js.sabae.cc/CSV.js";
-const data = await CSV.fetchJSON("kani-lunch-echizen.csv");
-data.forEach(d => {
-  d.緯度 = d.緯度.trim();
-  d.経度 = d.経度.trim();
-});
-await Deno.writeTextFile("test.csv", CSV.stringify(data));
